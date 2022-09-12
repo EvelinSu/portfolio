270c98fa8dd487e604ce8c125c78a7078254bd6b
EvelinSu <firest-evi@mail.ru>
2022-09-12 23:42:52 +0300

    added portfolio page

---
+++ src/Pages/PortfolioPage/PortfolioBlock.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import s from "./styles.module.css";
+import Button from "../../components/Button/Button";
+
+type TypePortfolioBlockProps = {
+    title: string,
+    description: string,
+    image: string
+    link?: string
+}
+
+const PortfolioBlock: React.FC<TypePortfolioBlockProps> = (props) => {
+    return (
+        <div className={s.blockContainer}>
+            <div className={s.body}>
+                <img className={s.image} src={props.image}/>
+                <div className={s.shadow}>
+                    <Button target="_blank" href={props.link} className={s.button} label={"Смотреть"}/>
+                </div>
+            </div>
+            <div className={s.description}>
+                <h1 className={s.title}>
+                    {props.title}
+                </h1>
+                <p>
+                    {props.description}
+                </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default PortfolioBlock;
--- src/Pages/PortfolioPage/PortfolioPage.tsx
+++ src/Pages/PortfolioPage/PortfolioPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {TPageProps} from "../../types";
+import s from "./styles.module.css"
+import PortfolioBlock from "./PortfolioBlock";
 
 const PortfolioPage: React.FC<TPageProps> = (props) => {
     return (
@@ -7,8 +9,43 @@ const PortfolioPage: React.FC<TPageProps> = (props) => {
             <h1 className={"sectionTitle"}>
                 My works
             </h1>
+            <div className={s.flex}>
+                {works.map((el, i) => (
+                    <PortfolioBlock
+                        key={i}
+                        title={el.title}
+                        link={el.link}
+                        description={el.description}
+                        image={el.image}
+                    />
+                ))}
+            </div>
         </section>
     );
 };
 
 export default PortfolioPage;
+
+const works = [
+    {
+        title: 'Jolly, sunny pirate',
+        image: 'https://i.imgur.com/iapZ8JD.png',
+        description: 'Ships fall with urchin at the undead prison! Aye! Pieces o hunger are forever rough. Jolly,' +
+            ' scrawny seashell. go to cabo rojo.',
+        link: '//www.google.com'
+    },
+    {
+        title: 'Greed, endurance ',
+        image: 'https://i.imgur.com/Dlh1bqt.png',
+        description: 'Ho-ho-ho! strength of malaria. Arrr, never crush a freebooter. Lubbers are the seas of the sunny love. Jolly, yer not' +
+            ' blowing me without a desolation!',
+        link: '//www.google.com'
+    },
+    {
+        title: 'Yellow fever, love',
+        image: 'https://i.imgur.com/8T4DKS2.png',
+        description: 'The swashbuckling captain darkly trades the wind. Oh! Pieces o life are forever wet. Well, big' +
+            ' girl. go to la marsa beach.',
+        link: '//www.google.com'
+    }
+]
