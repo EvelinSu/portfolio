8b8ecc7e96d04289248173ee7c5584c9af4eb3d5
EvelinSu <firest-evi@mail.ru>
2022-11-03 01:31:10 +0300

    - added particle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './styles/App.sass';
 import HomePage from "./Pages/HomePage/HomePage";
 import MySkillsPage from "./Pages/MySkillsPage/MySkillsPage";
@@ -6,26 +6,31 @@ import PortfolioPage from "./Pages/PortfolioPage/PortfolioPage";
 import ContactsPage from "./Pages/ContactsPage/ContactsPage";
 import Sidebar from "./Layout/Sidebar/Sidebar";
 import InfoBlock from "./Layout/InfoBlock/InfoBlock";
+import {ParticlesContainer} from "./components/Particle/Particle";
 
 export const SECTIONS = ["Home", "Skills", "Portfolio", "Contacts"]
 
 function App() {
-    useEffect(() => {
-        alert('my portfolio in progress ｡ﾟ(｡ﾉωヽ｡)ﾟ｡')
-    }, [])
+
+    // useEffect(() => {
+    //     alert('my portfolio in progress ｡ﾟ(｡ﾉωヽ｡)ﾟ｡')
+    // }, [])
+
     return (
         <div className="site__wrapper">
+            <ParticlesContainer />
             <Sidebar sections={SECTIONS} />
             <div className={"sections__container"}>
                 <HomePage id={SECTIONS[0]} />
                 <MySkillsPage id={SECTIONS[1]} />
                 <PortfolioPage id={SECTIONS[2]} />
                 <InfoBlock />
-                <ContactsPage id={SECTIONS[3]} />
+                <ContactsPage id={SECTIONS[3]}/>
                 <div className={"footer"}>
                     Copyright © 2022. All Rights Reserved.
                 </div>
             </div>
+
         </div>
     );
 }
